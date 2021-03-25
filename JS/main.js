0cf5b4fd548510d571eb0f23570053184ac327a1
Riza Karabicek <302713214@student.rocmondriaan.nl>
2021-03-25 10:55:48 +0100

    gekke dark theme

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,41 @@
 //Dit is een thema switcher voor de website
 
-const button = document.querySelector(".light-on-button");
+function black() {
+        const bodyTheme = document.body;
+        bodyTheme.classList.toggle("dark-mode");
+};
 
-button.addEventListener("click", function() {
-    const elements = document.querySelectorAll(".themed");
 
-    for (let i = 0; i< elements.length; i++) {
-        const element = elements[i];
-        element.classList.toggle("theme-light");
-    }
-});
+function dayToHour() {
+    let days = document.getElementById("numberDays").value;
+    document.getElementById("hours").innerHTML = days / 24;
+    console.log(days, hours);
+  };
+
+  let mode = localStorage.getItem("mode");
+
+  const chooseLightSide = () => {
+      document.classList.add('light-mode');
+      document.classList.add('dark-mode');
+      localStorage.setItem('mode', 'light-mode');
+  };
+
+  const chooseDarkSide = () => {
+    document.classList.add('dark-mode');
+    document.classList.add('light-mode');
+    localStorage.setItem('mode', 'dark-mode');
+};
+
+const chooseSide = () => {
+    document.classList.add('dark-mode');
+    document.classList.add('light-mode');
+    localStorage.setItem('mode', 'choose');
+};
+
+if (mode == 'light-mode') {
+    chooseLightSide();
+} else if (mode == 'dark-mode') {
+    chooseDarkSide();
+} else {
+    localStorage.setItem('mode', 'choose')
+}
