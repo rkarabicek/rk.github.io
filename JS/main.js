66f45d8724dddb2305efec4a231ccd95f0334b61
Riza Karabicek <302713214@student.rocmondriaan.nl>
2021-04-01 10:54:53 +0200

    kanker

--- JS/main.js
+++ JS/main.js
@@ -12,30 +12,120 @@ function dayToHour() {
     console.log(days, hours);
   };
 
-  let mode = localStorage.getItem("mode");
-
-  const chooseLightSide = () => {
-      document.classList.add('light-mode');
-      document.classList.add('dark-mode');
-      localStorage.setItem('mode', 'light-mode');
+function hourToDay() {
+    let days = document.getElementById("numberHours").value;
+    document.getElementById("days").innerHTML = days * 24;
+    console.log(days, hours);
   };
 
-  const chooseDarkSide = () => {
-    document.classList.add('dark-mode');
-    document.classList.add('light-mode');
-    localStorage.setItem('mode', 'dark-mode');
-};
-
-const chooseSide = () => {
-    document.classList.add('dark-mode');
-    document.classList.add('light-mode');
-    localStorage.setItem('mode', 'choose');
-};
-
-if (mode == 'light-mode') {
-    chooseLightSide();
-} else if (mode == 'dark-mode') {
-    chooseDarkSide();
-} else {
-    localStorage.setItem('mode', 'choose')
-}
+  const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+  
+  function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+  
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue =
+        displayValue === '0' ? digit : displayValue + digit;
+    }
+  }
+  
+  function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) {
+      calculator.displayValue = '0.';
+      calculator.waitingForSecondOperand = false;
+      return;
+    }
+  
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+  }
+  
+  function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+  
+    if (operator && calculator.waitingForSecondOperand) {
+      calculator.operator = nextOperator;
+      return;
+    }
+  
+    if (firstOperand == null && !isNaN(inputValue)) {
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+      const currentValue = firstOperand || 0;
+      const result = calculate(currentValue, inputValue, operator);
+  
+      calculator.displayValue = String(result);
+      calculator.firstOperand = result;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+  }
+  
+  function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }
+  
+    return secondOperand;
+  }
+  
+  function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+  }
+  
+  function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+  }
+  
+  updateDisplay();
+  
+  const keys = document.querySelector('.calculator-keys');
+  keys.addEventListener('click', event => {
+    const { target } = event;
+    if (!target.matches('button')) {
+      return;
+    }
+  
+    if (target.classList.contains('operator')) {
+      handleOperator(target.value);
+      updateDisplay();
+      return;
+    }
+  
+    if (target.classList.contains('decimal')) {
+      inputDecimal(target.value);
+      updateDisplay();
+      return;
+    }
+  
+    if (target.classList.contains('all-clear')) {
+      resetCalculator();
+      updateDisplay();
+      return;
+    }
+  
+    inputDigit(target.value);
+    updateDisplay();
+  });
+  
