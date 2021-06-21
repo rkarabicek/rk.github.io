8e78470f542e0613cc6e7e2c5ad3e26920d74e4b
Riza Karabicek <302713214@student.rocmondriaan.nl>
2021-06-21 09:16:45 +0200

    huge update

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,19 @@
 // Theme switcher
+isWhite = true;
+
 function black() {
   const bodyTheme = document.body;
   bodyTheme.classList.toggle("dark-mode");
+  if (isWhite == true) {
+    document.querySelector(".grid-container > div").style.backgroundColor = "pink";
+    isWhite = false
+  } else {
+    document.querySelector(".grid-container > div").style.backgroundColor = "white";
+    isWhite = true
+  }
 };
 
+//rekenmachine
 window.onload = function () {
   var buttons = document.getElementsByTagName('span'),
     result = document.querySelectorAll('.result p')[0],
